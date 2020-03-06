6e1eea3c95e6914cf6851c8bde137b5a62af1271
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-03-06 11:25:05 +0100

    Initial commit

---
+++ guidelines.js
@@ -0,0 +1,11 @@
+var Guidelines = Object.getPrototypeOf(app).Guidelines = new Component("guidelines");
+Guidelines.debug = true;
+Guidelines.createdAt      = "2.0.0";
+Guidelines.lastUpdate     = "2.0.0";
+Guidelines.version        = "1";
+// Guidelines.factoryExclude = true;
+// Guidelines.loadingMsg     = "This message will display in the console when component will be loaded.";
+
+// Guidelines.prototype.onCreate = function(){
+  // do thing after element's creation
+// }
